69f3d3e6e1230b81f1683f80c59d336d2b8a81f0
2mas1belino <tomasumbelino03@gmail.com>
2025-06-25 16:42:30 +0100

    feat: implement add operation in CalculatorServiceImpl

---
+++ src/main/java/com/tomas/calculator_api/calculator_api/CalculatorServiceImpl.java
@@ -0,0 +1,14 @@
+package com.tomas.calculator_api.calculator_api;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+    @Override
+    public BigDecimal add(BigDecimal a, BigDecimal b) {
+        return a.add(b);
+    }
+}
+
