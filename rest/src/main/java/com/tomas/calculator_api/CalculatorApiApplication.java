2f3bceb6267f5e89bbd41f065626dcb682512108
2mas1belino <tomasumbelino03@gmail.com>
2025-06-30 15:01:19 +0100

    feat: add Dockerfiles and docker-compose

--- rest/src/main/java/com/tomas/calculator_api/CalculatorApiApplication.java
+++ rest/src/main/java/com/tomas/calculator_api/CalculatorApiApplication.java
@@ -1,5 +1,6 @@
 package com.tomas.calculator_api;
 
+import com.tomas.calculator_api.controller.CalculatorController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
